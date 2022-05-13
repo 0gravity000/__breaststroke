58e2942ae1f52c2b736a973e701a045b89541c3f
0gravity000 <0gravit000@gmail.com>
2022-05-13 20:35:43 +0900

    NavbarMainとSidebarHomeを追加 Bootstarapをnpm install

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,36 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <NavbarMain />
+  <div class="container">
+    <div class="row">
+      <div class="col-3">
+        <SidebarHome />
+      </div>
+      <div class="col-9">
+        <router-view/>
+      </div>
+    </div>
+  </div>
 </template>
+
+<script>
+// @ is an alias to /src
+import NavbarMain from '@/components/NavbarMain.vue'
+import SidebarHome from '@/components/SidebarHome.vue'
+
+export default {
+  components: {
+    NavbarMain,
+    SidebarHome,
+  },
+}
+
+</script>
 
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
 }
 
---
+++ src/components/SidebarHome.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="sidebar-home">
+    <ul class="list-group list-group-flush">
+      <li class="list-group-item">
+        <router-link to="/">実験01</router-link>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
